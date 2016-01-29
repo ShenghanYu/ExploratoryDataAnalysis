5e52da130c14a3f45d7922e78b530e1060fa5369
amirosimani <amirhs.imani@gmail.com>
2016-01-29 10:40:14 -0500

    import file from github
    read CSV file from github instead of local XLS file

--- Project01.R
+++ Project01.R
@@ -1,15 +1,15 @@
 # Set local work directory 
 setwd("./ExploratoryDataAnalysis")
 
-# Installing all the required packages to read xlsx files
-library(readxl)
+# Installing all the required packages to read CSV files
+library(RCurl)
 
-# Read  file from the local directory
-# to change: read it from GitHub instead of local 
-df = read_excel("Survey.xlsx")
+# Read  file from GitHub repo
+CSV_url <- getURL("https://raw.githubusercontent.com/Amirosimani/ExploratoryDataAnalysis/master/Survey.csv")
+DataFrame <- read.csv(text = CSV_url)
 
-# Delete empty columns 
-df <- Filter(function(x)!all(is.na(x)), df)
+# Delete empty columns in data frame
+DataFrame <- Filter(function(x)!all(is.na(x)), DataFrame)
 
 " Split tools in column 3
 sapply(df, strsplit(df$'Experiences with tools', ","))
