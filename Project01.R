facc910b64b14bb0c340a499d3bb8abe8d202db0
amirosimani <amirhs.imani@gmail.com>
2016-01-29 20:42:18 -0500

    Clean up version of data
    Ready for further analysis

--- Project01.R
+++ Project01.R
@@ -1,16 +1,28 @@
 # Set local work directory 
 setwd("./ExploratoryDataAnalysis")
 
-# Installing all the required packages to read CSV files
-library(RCurl)
+# Installing all the required packages
+library(RCurl)   #to read CSV
+library(data.table)  #Data table manipulation
+
 
 # Read  file from GitHub repo
 CSV_url <- getURL("https://raw.githubusercontent.com/Amirosimani/ExploratoryDataAnalysis/master/Survey.csv")
 DataFrame <- read.csv(text = CSV_url)
 
-# Delete empty columns in data frame
-DataFrame <- Filter(function(x)!all(is.na(x)), DataFrame)
+## Tidying the data frame
+DataFrame <- Filter(function(x)!all(is.na(x)), DataFrame) #Delete empty columns
+colnames(DataFrame) <- c("waitlist","degree","tools", 
+                         "R_DataManipulation", "pronoun","editor","R_Graphics",
+                         "R_MultiVariate","Markdown","Matlab_DataManipulation","GitHub") #Rename columns name
+# cleaning up text
+DataFrame$tools = gsub("\\(formerly docs\\)", "", DataFrame$tools)
+DataFrame$tools =  gsub("\\(grep\\)", "", gsub("\\(terminal \\/ command line\\)","", DataFrame$tools))
+                    
+#splitting programs into a list
+tools <- strsplit(as.character(DataFrame$tools),',') 
+setDT(DataFrame)[, paste0("tools", 1:16) := tstrsplit(tools, ",")]   #Add seperate column for each program
 
-" Split tools in column 3
-sapply(df, strsplit(df$'Experiences with tools', ","))
- "      
+
+
+
