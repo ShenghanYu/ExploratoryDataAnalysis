6b7421da1f4ba4be1a829a8369d658706e3a45c0
amirosimani <amirhs.imani@gmail.com>
2016-01-31 23:36:31 -0500

    first attempt to create graphs

--- Project01.R
+++ Project01.R
@@ -1,13 +1,11 @@
-# Set local work directory 
-setwd("./GitHub/ExploratoryDataAnalysis")
-
 # Installing all the required packages
-needed.libraries <- c("RCurl", "data.table")
+needed.libraries <- c("RCurl", "data.table", "ggplot2")
 install.packages(needed.libraries, dependencies = TRUE)
 
 # Run packages
 library(RCurl)   #to read CSV
 library(data.table)  #Data table manipulation
+library(ggplot2)
 
 # Read  file from GitHub repo
 CSV_url <- getURL("https://raw.githubusercontent.com/Amirosimani/ExploratoryDataAnalysis/master/Survey.csv")
@@ -26,6 +24,10 @@ DataFrame$tools =  gsub("\\(grep\\)", "", gsub("\\(terminal \\/ command line\\)"
 tools <- strsplit(as.character(DataFrame$tools),',') 
 setDT(DataFrame)[, paste0("tools", 1:16) := tstrsplit(tools, ",")]   #Add seperate column for each program
 
+# Converting columns to factor
+col_names <- names(DataFrame)
+DataFrame[,col_names] <- lapply(DataFrame[,col_names] , factor)
 
-
-
+#producing graphs
+c <- ggplot(DataFrame, aes(factor(DataFrame$waitlist)))
+c + geom_bar(width=.5)
